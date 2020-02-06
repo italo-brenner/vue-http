249130d8ae1664717663670c0b26cb32e4f7fa3e
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-02-06 15:37:19 -0300

    Ordenando por tarefas nao concluidas

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -18,7 +18,7 @@
 
       <ul class="list-group" v-if="tarefas.length > 0">
           <TarefasListaIten
-              v-for="tarefa in tarefas"
+              v-for="tarefa in tarefasOrdenadas"
               :key="tarefa.id"
               :tarefa="tarefa"
               @editar="selecionarTarefaParaEdicao"
@@ -56,6 +56,20 @@ export default {
             exibirFormulario: false,
             tarefaSelecionada: undefined
         }
+    },
+    computed: {
+      tarefasOrdenadas() {
+        return this.tarefas.slice().sort((t1, t2) => {
+          if (t1.concluido === t2.concluido) {
+            return t1.titulo < t2.titulo
+              ? -1
+              : t1.titulo > t2.titulo
+                ? 1
+                : 0
+          }
+          return t1.concluido - t2.concluido
+        })
+      }
     },
     created() {
       axios.get(`${config.apiURL}/tarefas`)
