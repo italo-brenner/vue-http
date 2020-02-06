c542ce4cfd43d5ba187b7acd84db53df70968f2a
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-02-06 11:11:42 -0300

    Requisicao GET para buscar lista de tarefas

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -1,24 +1,27 @@
 <template>
     <div>
 
-        <h1 class="font-weight-light">Lista de Tarefas</h1>
+      <h1 class="font-weight-light">Lista de Tarefas</h1>
 
-        <ul class="list-group" v-if="tarefas.length > 0">
-            <TarefasListaIten
-                v-for="tarefa in tarefas"
-                :key="tarefa.id"
-                :tarefa="tarefa" />
-        </ul>
+      <ul class="list-group" v-if="tarefas.length > 0">
+          <TarefasListaIten
+              v-for="tarefa in tarefas"
+              :key="tarefa.id"
+              :tarefa="tarefa" />
+      </ul>
 
-        <p v-else>Nenhuma tarefa criada.</p>
+      <p v-else>Nenhuma tarefa criada.</p>
 
-        <TarefaSalvar />
+      <TarefaSalvar />
 
     </div>
 </template>
 
 <script>
 
+import axios from 'axios'
+
+import config from './../config/config'
 import TarefaSalvar from './TarefaSalvar.vue'
 import TarefasListaIten from './TarefasListaIten.vue'
 
@@ -30,11 +33,16 @@ export default {
     data() {
         return {
             tarefas: [
-                { id: 1, titulo: 'Aprender JavaScript', concluido: true },        
-                { id: 2, titulo: 'Aprender Vue', concluido: true },
-                { id: 3, titulo: 'Aprender Axios', concluido: false }
+                
             ]
         }
+    },
+    created() {
+      axios.get(`${config.apiURL}/tarefas`)
+        .then((response) => {
+          console.log(response)
+          this.tarefas = response.data
+        })
     }
 }
 </script>
