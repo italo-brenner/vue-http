232d601c9727b4ca21ed116ccda156d39349e737
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-02-07 10:02:59 -0300

    Interceptando requisicoes

--- src/axios.js
+++ src/axios.js
@@ -5,4 +5,25 @@ const instance = axios.create({})
 
 instance.defaults.baseURL = config.apiURL
 
+instance.interceptors.request.use(config => {
+    console.log('Interceptando requisição: ', config)
+    config.data = {
+        ...config.data,
+        curso: 'Vue JS'
+    }
+    return new Promise((resolve, reject) => {
+        console.log('Fazendo requisicao aguardar...')
+        setTimeout(() => {
+            console.log('Fazendo requisicao aguardar...')
+            setTimeout(() => {
+                console.log('Enviando requisição...')
+                resolve(config)
+            }, 2000)
+        })
+    })
+}, error => {
+    console.log('Erro ao fazer requisicao: ', error)
+    return Promise.reject(error)
+})
+
 export default instance
